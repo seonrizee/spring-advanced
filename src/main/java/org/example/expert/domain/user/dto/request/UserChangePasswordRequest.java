5f9cb062d4a87b81d85a9db753ff292c761cb4ca
seonrizee <seonrizee@gmail.com>
2025-09-01 02:09:23 +0900

    refactor(lv6): 비밀번호 변경 요청 및 사용자 역할 변경 요청에 유효성 검사 강화 및 메시지 추가

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -2,21 +2,24 @@ package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Length;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserChangePasswordRequest {
 
-    @NotBlank
+    @NotBlank(message = "기존 비밀번호는 필수입니다.")
     private String oldPassword;
 
-    @NotBlank
-    @Length(min = 8, message = "새 비밀번호는 8자 이상이어야 합니다.")
-    @Pattern(regexp = "^(?=.*\\d)(?=.*[A-Z]).*$", message = "새 비밀번호는 숫자와 대문자를 포함해야 합니다.")
+    @NotBlank(message = "새 비밀번호는 필수입니다.")
+    @Size(min = 8, max = 20, message = "새 비밀번호는 8자 이상 20자 이하여야 합니다.")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            message = "새 비밀번호는 대소문자, 숫자, 특수문자를 포함해야 합니다."
+    )
     private String newPassword;
 }
--- src/main/java/org/example/expert/domain/user/dto/request/UserRoleChangeRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserRoleChangeRequest.java
@@ -1,5 +1,7 @@
 package org.example.expert.domain.user.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,5 +11,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserRoleChangeRequest {
 
+    @NotBlank(message = "사용자 역할은 필수입니다.")
+    @Pattern(
+            regexp = "^(USER|ADMIN)$",
+            message = "사용자 역할은 USER 또는 ADMIN만 가능합니다."
+    )
     private String role;
 }
